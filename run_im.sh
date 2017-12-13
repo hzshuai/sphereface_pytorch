09e73a56da3c6fa3d819d78752cba956268a78e7
zhangshuaihuang <zhangshuaihuang@creditease.cn>
2017-12-13 22:15:17 +0800

    add sh for ms celeb 1m

---
+++ run_im.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export PATH=/Users/hzshuai/anaconda3/envs/python27/bin
+python -u train.py --dataset=/Users/hzshuai/PythonProjects/ai-playground/casia-faces/CASIA-maxpy-clean --maxlistnum=300
+
+#export PATH=/data/huangzhangshuai/anaconda3/envs/python27/bin
+#python -u train.py \
+#--lr=0.1 \
+#--dataset=/data/huangzhangshuai/anaconda3/workspace/dataset/CASIA-maxpy-clean \
+#> log-1213_v1.log 2>&1 &
+
+
+#ps aux | grep train.py | awk '{print $2}' | xargs kill
+# eval lfw
+# python -u lfw_eval.py --model=sphere20a_19.pth --lfw=/data/Datasets/msra_lfw/lfw/lfw.zip >> lfw_eval.log
