023e9c095483547e917bf181e2f174193280ad82
zhangshuaihuang <zhangshuaihuang@creditease.cn>
2017-12-18 21:44:36 +0800

    fix run

--- run_ms_align.sh
+++ run_ms_align.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 python27 -u ms_celeb_align_112x96.py --lfw=1 --nproc=2 \
---data-dir=/data/Datasets/msra_lfw/lfw \
---lmk=lfw_lmk \
+--data-dir=/data/Datasets/msra_lfw/lfw/lfw \
+--lmk=data/lfw_lmk \
 --output-dir=lfw_112x96 \
 --print-inter=5 \
 > log-align.log 2>&1 &
